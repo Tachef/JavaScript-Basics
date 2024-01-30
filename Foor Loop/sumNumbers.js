b2c9ddefa4dc67cc149224751b00ace19066ee0e
Stanislav Ganev <114296698+Tachef@users.noreply.github.com>
2024-01-30 22:02:11 +0200

    Add files via upload

---
+++ Foor Loop/sumNumbers.js
@@ -0,0 +1,10 @@
+function sumNumbers(input) {
+    let numAsString = String(input[0]);
+    let sum = 0;
+    for (let i = 0; i < numAsString.length; i++ ) {
+        let num = Number(numAsString[i])
+        sum += num;
+    }
+    console.log(`The sum of the digits is:${sum}`)
+}
+sumNumbers(["1234"]);
